75320ff1733860212c8e54af467ed43017919944
gabriellichacz <gabriellichacz@gmail.com>
2022-05-31 00:06:03 +0200

    project completed

--- Hellwig.r
+++ Hellwig.r
@@ -1,25 +1,25 @@
 #Hellwig-Total
-#@Autor, napisa³, opisa³, przetestowa³: Napora Jaros³aw
+#@Autor, napisa?, opisa?, przetestowa?: Napora Jaros?aw
 
 #dane najpierw x pozniej y
 
 #----- Hellwig z danych ----
 library(readxl)
-Dane=read_excel("Obliczenia.xlsx", sheet=3,range="A1:E14") #zmieñ zakresy
+Dane=read_excel("Excel_obliczenia.xlsx", sheet=2, range="A1:I18") #zmie? zakresy
 
-N=length(Dane)-1     #oblicza iloœæ zmiennych objaœniaj¹cych
-M=2^N-1              #oblicza iloœæ kombinacji 0-1
+N=length(Dane)     #oblicza ilo?? zmiennych obja?niaj?cych
+M=2^N-1              #oblicza ilo?? kombinacji 0-1
 
 
-zm_obj=Dane[,1:N]    #tworzy macierz zmiennych objaœnij¹cych
-r=cor(zm_obj)               #tworzy macierz korelacji miêdzy zmiennymi
-r=as.matrix(abs(r))         #tworzy wartoœci bezwzgledne i macierz 
-R=cor(zm_obj,Dane[,N+1])    #tworzy wektor korelacji Y z kazd¹ ze zmiennych
+zm_obj=Dane[,1:N]    #tworzy macierz zmiennych obja?nij?cych
+r=cor(zm_obj)               #tworzy macierz korelacji mi?dzy zmiennymi
+r=as.matrix(abs(r))         #tworzy warto?ci bezwzgledne i macierz 
+R=cor(zm_obj,dane_y)    #tworzy wektor korelacji Y z kazd? ze zmiennych
 R=as.vector(R)              #zapisuje wektor jako wektor
 
 tab=as.matrix(expand.grid(rep(list(0:1), N)))[-1,]     #tworzy macierz 0-1 kombinacji
 colnames(tab)=colnames(Dane)[1:N]
-wyniki=matrix(0,M,N)        #tworzy macierz 0 na wyniki cz¹stkowe pojemnosci
+wyniki=matrix(0,M,N)        #tworzy macierz 0 na wyniki cz?stkowe pojemnosci
 colnames(wyniki)=colnames(Dane)[1:N]
 for(i in 1:M)
 {
@@ -32,12 +32,12 @@ for(i in 1:M)
 maks=which.max(rowSums(wyniki))
 tab[maks,]
 
-#Podgl¹d kilku najlepszych wyników
+#Podgl?d kilku najlepszych wynik?w
 wynikiS=cbind(wyniki,0)
 wynikiS[,(N+1)]=rowSums(wyniki)
 nazwy=colnames(as.data.frame(wyniki))
 colnames(wynikiS)=c(nazwy,"hellwigP")
 
-ind=order(wynikiS[,(N+1)],decreasing = TRUE)[1:15] #zwróci 15 najleprzych
+ind=order(wynikiS[,(N+1)],decreasing = TRUE)[1:15] #zwr?ci 15 najleprzych
 najlepsze15=wynikiS[ind,]
 
